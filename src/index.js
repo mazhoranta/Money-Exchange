c17e9c8d5e64329b09509dc87df6eff1b6180462
mazhoranta <mazhoranta@gmail.com>
2018-09-19 00:40:12 +0300

    write function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var coins = {};
+
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else if (currency >= 1 && currency <= 10000) {
+        const halfDollars = Math.trunc(currency / 50);
+        const quarters = Math.trunc((currency % 50) / 25);
+        const dimes = Math.trunc((currency % 50 % 25) / 10);
+        const nickels = Math.trunc((currency % 50 % 25 % 10) / 5);
+        const pennies = currency % 50 % 25 % 10 % 5;
+
+        if (halfDollars > 0)
+            coins.H = halfDollars;
+        if (quarters > 0)
+            coins.Q = quarters;
+        if (dimes > 0)
+            coins.D = dimes;
+        if (nickels > 0)
+            coins.N = nickels;
+        if (pennies > 0)
+            coins.P = pennies;
+    
+        return coins;
+    } else {
+        return {};
+    }
 }
